54b1e387dd22ea8ef5abc49774909af92aec1774
Jonathan King <jonathanlking@me.com>
2022-08-14 17:20:13 +0100

    Upgrade nix packages

--- nix/tooling/default.nix
+++ nix/tooling/default.nix
@@ -8,14 +8,14 @@
 let
   # The version of Nixpkgs we will pin for our tooling.
   baseNixpkgs = builtins.fetchTarball {
-    name = "nixos-unstable-2020-06-22";
+    name = "nixos-unstable-2022-01-14";
 
     # Update the "name" attribute if/when you change this
-    url = https://github.com/NixOS/nixpkgs/archive/dca182df882db483cea5bb0115fea82304157ba1.tar.gz;
+    url = https://github.com/NixOS/nixpkgs/archive/665a4df77bf59dce04a00bdd4620aba43d4910bd.tar.gz;
 
     # You can obtain an appropriate hash using
     # `nix-prefetch-url --unpack <url>`.
-    sha256 = "0193bpsg1ssr93ihndyv7shz6ivsm8cvaxxl72mc7vfb8d1bwx55";
+    sha256 = "19ahdbfsi940fwjzjgszbpz51qn78v8ql10gfndaz5cvqd1ks6l2";
   };
 
   # If you need a different version or a custom build of a tool, or perhaps a
